459431be462cffad0f44e3af8b8386a8d7880b5c
SaifulIslam <msik.milon@gmail.com>
2025-01-23 14:23:48 -0800

    finish profile editing option

--- FuliangMart/userauths/views.py
+++ FuliangMart/userauths/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
-from userauths.forms import UserRegisterForm
+from userauths.forms import UserRegisterForm, ProfileForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.conf import settings
-from userauths.models import User 
+from userauths.models import User, Profile
 # User = settings.AUTH_USER_MODEL #this is for login system
+
 
 # Create your views here.
 def register_view(request):
@@ -52,8 +53,27 @@ def login_view(request):
     return render(request,"userauths/sign-in.html") # {"email":email}
 
 
-
 def logout_view(request):
     logout(request)
     messages.success(request, "You logged out.")
     return redirect("userauths:sign-in")
+
+
+def profile_edit(request):
+    profile = Profile.objects.get(user=request.user)
+
+    if request.method == "POST":
+        profile_save = ProfileForm(request.POST, request.FILES, instance=profile)
+        if profile_save.is_valid():
+            profile_save = profile_save.save(commit=False)
+            profile_save.user = request.user
+            profile_save.save()
+            messages.success(request, "Profile Updated Successfully.")
+            return redirect("martApp:dashboard")
+    else:
+            profile_save = ProfileForm(instance=profile)
+    context = {
+        "form":profile_save,
+        "profile":profile,
+    }
+    return render(request, "userauths/profile-edit.html", context)
